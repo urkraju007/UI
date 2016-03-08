c4cfde6fec216b3796bd8b0dd45d37da802a8165
urkraju007 <urkraju007@gmail.com>
2016-03-08 00:47:45 -0500

    Updated

--- EmployeeInfo.js
+++ EmployeeInfo.js
@@ -1,22 +1,53 @@
 
-
+var employee=[];
+var i=0;
+var d=0;
 function getEmployee(){
 	
-	var employee=["Mike","Kelly",34,45000,"Services"];
-	document.getElementById("firstname").value=employee[0];
-	document.getElementById("lastname").value=employee[1];
-	document.getElementById("age").value=employee[2];
-	document.getElementById("salary").value=employee[3];
-	document.getElementById("department").value=employee[4];
+	
+	i=employee.length-1;
+	if(d==0 || d<=i){
+		if(d!=0)
+			i=i-d;	
+		document.getElementById("department").value=employee[i];
+		i--;
+		document.getElementById("salary").value=employee[i];
+		i--;
+		document.getElementById("age").value=employee[i];
+		i--;
+		document.getElementById("lastname").value=employee[i];
+		i--;
+		document.getElementById("firstname").value=employee[i];
+		i--;
+		d=d+5;
+	}
+	else 
+		reseting();
+	
 }
-
 function addEmployee()
-{
-	var firstName = document.getElementById("firstname").value;
-	var lastName = document.getElementById("lastname").value;
-	var Age = document.getElementById("age").value;
-	var salary = document.getElementById("salary").value;
-	var salary = document.getElementById("salary").value;
+{		
+	i=employee.length;
+	employee[i] = document.getElementById("firstname").value;
+	i++;
+	employee[i] = document.getElementById("lastname").value;
+	i++;
+	employee[i] = document.getElementById("age").value;
+	i++;
+	employee[i] = document.getElementById("salary").value;
+	i++;
+	employee[i] = document.getElementById("department").value;
+	i++;
+	d=0;
+	reseting();
+	
+}
+function reseting(){
+	document.getElementById("department").value="";
+	document.getElementById("salary").value="";
+	document.getElementById("age").value="";
+	document.getElementById("lastname").value="";
+	document.getElementById("firstname").value="";
 }
 
 
--- createTable.js
+++ createTable.js
@@ -15,14 +15,19 @@ createTable("input","salary");
 createTable("br");
 createTable("input","Department");
 createTable("input","department");
-
+reseting();
 function createTable(type,value1){
-	
-	
 	var employeetab=document.getElementById("employee");
 	var tagc = document.createElement(type);
 	tagc.value=value1;
 	tagc.id=value1;
 	employeetab.appendChild(tagc);
 
+}
+function reseting(){
+	document.getElementById("department").value="";
+	document.getElementById("salary").value="";
+	document.getElementById("age").value="";
+	document.getElementById("lastname").value="";
+	document.getElementById("firstname").value="";
 }
